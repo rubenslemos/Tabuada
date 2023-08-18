4e1947fc755cd7db2433da1984f0ab63be347414
Rubens <rubenslemos@gmail.com>
2023-08-18 07:31:12 -0300

    tabuada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,42 @@
-document.addEventListener('click', async e => {
-  const el = e.target;
-  const tag = el.tagName.toLowerCase();
-  if (tag === 'a') {
-    e.preventDefault();
-    carregaPagina(el);
-  }
-});
 
-const carregaPagina = async el => {
-  try {
-    const href = el.getAttribute('href');
-    const response = await fetch(href);
-    if (response.status !== 200) throw new Error('Error 404 nosso');
-    const html = await response.text();
-    carregaResultado(html);
-  } catch (e) {
-    console.error(e);
-  }
-};
 
-const carregaResultado = response => {
-  const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = response;
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index/10 +0.3}s`);
+    });
+  }
+
+  handleClick = () => {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    return this;
+  }
 }
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+
+mobileNavbar.init();
