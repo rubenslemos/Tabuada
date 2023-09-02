625edb4cae36a6f17d244f5e644cc37139c69eaf
Rubens <rubenslemos@gmail.com>
2023-09-02 10:50:49 -0300

    atualização

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,40 @@
 const button = document.querySelector('.resposta')
 const modal = document.querySelector('.dialog')
-const numerador = document.querySelector('.numerador');
-const denominador = document.querySelector('.denominador');
+const numerador = document.querySelector('.numerador')
+const denominador = document.querySelector('.denominador')
 const resultado = document.getElementById("resultado")
 const fechar = document.querySelector('.fechar')
 const sucesso = document.querySelector('.sucess')
 const proxima = document.querySelector('.proxima')
 const anterior = document.querySelector('.anterior')
+const soma = document.querySelector('.soma')
+const itensSoma = document.querySelector('.itensSoma')
+const menos =document.querySelector('.menos')
+const itensMenos = document.querySelector('.itensMenos')
+const vezes =document.querySelector('.vezes')
+const itensVezes = document.querySelector('.itensVezes')
+const dividir =document.querySelector('.dividir')
+const itensDividir = document.querySelector('.itensDividir')
+const todas =document.querySelector('.todas')
+const itensTodas = document.querySelector('.itensTodas')
+const voltar = document.querySelector('.voltar')
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+    this.mobileMenu = document.querySelector(mobileMenu)
+    this.navList = document.querySelector(navList)
+    this.navLinks = document.querySelectorAll(navLinks)
+    this.activeClass = "active"
 
     if (this.mobileMenu) {
-      this.addClickEvent();
+      this.addClickEvent()
     }
   }
 
   animateLinks() {
     this.navLinks.forEach((link, index) => {
-      link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index/10 +0.3}s`);
-    });
+      link.style.animation ? (link.style.animation = "") :
+        (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 80 + 0.1}s`);
+    })
   }
 
   handleClick = () => {
@@ -46,7 +58,60 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li"
 );
 
+
+abrirSoma = () => {
+  soma.addEventListener('click', () => {
+    itensSoma.classList.toggle("mostra")
+    itensMenos.classList.remove("mostra")
+    itensVezes.classList.remove("mostra")
+    itensDividir.classList.remove("mostra")
+    itensTodas.classList.remove("mostra")
+  })
+}
+abrirSoma()
+abrirMenos = () => {
+  menos.addEventListener('click', () => {
+    itensMenos.classList.toggle("mostra")
+    itensSoma.classList.remove("mostra")
+    itensVezes.classList.remove("mostra")
+    itensDividir.classList.remove("mostra")
+    itensTodas.classList.remove("mostra")
+  })
+}
+abrirMenos()
+abrirVezes = () => {
+  vezes.addEventListener('click', () => {
+    itensVezes.classList.toggle("mostra")
+    itensSoma.classList.remove("mostra")
+    itensMenos.classList.remove("mostra")
+    itensDividir.classList.remove("mostra")
+    itensTodas.classList.remove("mostra")
+  })
+}
+abrirVezes()
+abrirDividir = () => {
+  dividir.addEventListener('click', () => {
+    itensDividir.classList.toggle("mostra")
+    itensSoma.classList.remove("mostra")
+    itensMenos.classList.remove("mostra")
+    itensVezes.classList.remove("mostra")
+    itensTodas.classList.remove("mostra")
+  })
+}
+abrirDividir()
+abrirTodas = () => {
+  todas.addEventListener('click', () => {
+    itensTodas.classList.toggle("mostra")
+    itensSoma.classList.remove("mostra")
+    itensMenos.classList.remove("mostra")
+    itensVezes.classList.remove("mostra")
+    itensDividir.classList.remove("mostra")
+  })
+}
+abrirTodas()
+
 mobileNavbar.init();
+
 criarNumerador = () => {
   numerador.innerHTML = Math.floor( ( Math.random() * ( 10 - 0 ) + 0 ) * 1 )
   return numerador;
@@ -58,7 +123,7 @@ criarDenominador = () => {
 criaTabuada = () => {
   criarNumerador();
   criarDenominador();
-  document.getElementById("resultado").value = null
+  resultado.value = null
 }
 criaTabuada();
 
@@ -86,7 +151,6 @@ button.onclick = function () {
 fechar.onclick = function () {
   modal.close()
 }
-
 proxima.onclick = function () {
   p[i] = Number(numerador.outerText);
   s[i] = Number(denominador.outerText);
@@ -94,21 +158,26 @@ proxima.onclick = function () {
   criaTabuada()
   modal.close()
 }
-anterior.onclick = function () {
-  numerador.innerText = p[i-1]
-  denominador.innerText = s[i - 1]
-  i--
-  if (i == 0) {
-    numerador.innerText = p[i]
-    denominador.innerText = s[i]
-    i++
-  }
-  document.getElementById("resultado").value = null
-  return numerador, denominador
+back = () => {
+    numerador.innerText = p[i-1]
+    denominador.innerText = s[i - 1]
+    i--
+    if (i == 0) {
+      numerador.innerText = p[i]
+      denominador.innerText = s[i]
+      i++
+    }
+    document.getElementById("resultado").value = null
 }
+anterior.onclick = back
 resultado.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     checaResultado()
     modal.showModal()
   }
 })
+
+voltar.onclick = () => {
+  resultado.value= null
+  modal.close()
+}
