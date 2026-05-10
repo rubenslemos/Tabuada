c39142eadec30c3da7a1d8a154511c69f6ca0196
Rubens <86129671+rubenslemos@users.noreply.github.com>
2026-05-09 23:20:52 -0300

    mailer ajuste

--- modules/mailer.js
+++ modules/mailer.js
@@ -2,19 +2,25 @@ const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
 const path = require('path')
 require('dotenv').config()
-const user = process.env.MAIL_USER
-const pass = process.env.MAIL_PASS
-const host = process.env.MAIL_HOST
-const port = process.env.MAIL_PORT
 
 const transport = nodemailer.createTransport({
-  host,
-  port,
-  auth: { user, pass },
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: { 
+    user: process.env.MAIL_USER, 
+    pass: process.env.MAIL_PASS 
+  },
 })
+
+// Configuração corrigida para Vercel
 transport.use('compile', hbs({
-  viewEngine: 'handlebars',
-  viewPath: path.resolve('./resources/mail/'),
-  extName:'.html',
+  viewEngine: {
+    extName: '.html',
+    partialsDir: path.resolve(__dirname, '../resources/mail/'), // Ajuste o nível se necessário
+    layoutsDir: path.resolve(__dirname, '../resources/mail/'),
+    defaultLayout: false, // Defina como false se não tiver um layout específico para o e-mail
+  },
+  viewPath: path.resolve(__dirname, '../resources/mail/'),
+  extName: '.html',
 }))
 module.exports = transport
